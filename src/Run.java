d72d1765da44ac0a101d770ca737fe54ee0b6d19
Thomas Jalabert <pol-jalabe@atlas.i3s.unice.fr>
2015-12-03 08:47:06 +0100

    COMMIT DE LA VIE

---
+++ src/Run.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import plague.inc.Entity.AbstractEntity;
+import plague.inc.Entity.Person;
+import plague.inc.Map;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Run {
+     public static void main(String[] args) {
+         Map map = new Map(5,10);
+         ArrayList<AbstractEntity> b = new ArrayList<>();
+         b.add(new Person());
+         b.add(new Person());
+         map.add(b);
+         map.print();
+         map.move();
+         map.print();
+         map.move();
+         map.print();
+         
+     }
+}
---
+++ src/plague/inc/Coordinates.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plague.inc;
+
+/**
+ * Classe coordonée 2D
+ * @author user
+ */
+public class Coordinates {
+    private int x,y;
+
+    /**
+     *
+     * @param x
+     * @param y
+     */
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getY() {
+        return y;
+    }
+    
+    /**
+     * Permet de calculer la distance manhatan entre deux coordonnées
+     * @param a la seconde coordonnée
+     * @return
+     */
+    public int distance(Coordinates a) {
+        return Math.abs(a.x - this.x) + Math.abs(a.y - this.y);
+    }
+    
+    
+    public int distance() {
+        return Math.abs(this.x) + Math.abs(this.y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coordinates other = (Coordinates) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        return this.y == other.y;
+    }
+    
+    
+    
+}
