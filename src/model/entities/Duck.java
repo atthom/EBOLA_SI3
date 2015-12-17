21914088a7cc669934b03d2e191ee947b33bd680
Thomas Jalabert <pol-jalabe@atlas.i3s.unice.fr>
2015-12-17 09:47:40 +0100

    supressions des fichiers de IntelliJ

--- src/model/entities/Duck.java
+++ src/model/entities/Duck.java
@@ -8,17 +8,10 @@ import model.virus.Virus;
  * Created by tibo on 17/12/15.
  */
 public class Duck extends FlyingThings {
-<<<<<<< HEAD
 
-	public Duck(Location location, Field field, Virus v) {
-		super(location, field, v);
-		// TODO Auto-generated constructor stub
-	}
-=======
-    
     public Duck(Location location, Field field, Virus v) {
         super(location, field, v);
+        // TODO Auto-generated constructor stub
     }
-    
->>>>>>> 42d440fdbe4c5a371cc5df9e23b6ad2aa6a93a37
+
 }
